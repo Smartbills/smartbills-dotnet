413f48527d6ea3d33b9761760cc53b81f046337d
Sébastien Labine <sebastien@smartbills.ca>
2022-03-18 14:47:21 -0400

    Added unit tests

---
+++ Smartbills.NET.UnitTests/Configuration/ServerFixture.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using Smartbills.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartbills.NET.IntegrationTests.Configuration
+{
+    public class ServerFixture : IDisposable
+    {
+        public ISmartbillsClient Client { get; private set; }
+
+        public ServerFixture()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+}
