be8ce2514e1eef63c1cf9ffb348a975b2e9118c1
Sébastien Labine <sebastien@smartbills.ca>
2022-12-05 17:11:04 -0500

    tests

--- Smartbills.NET.UnitTests/Services/BankClientTests.cs
+++ Smartbills.NET.UnitTests/Services/BankClientTests.cs
@@ -3,6 +3,7 @@ using Smartbills.NET.Entities.Banks;
 using Smartbills.NET.Infrastructure;
 using Smartbills.NET.Services.BankAccounts;
 using Smartbills.NET.Services.Banks;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -21,21 +22,36 @@ namespace Smartbills.NET.UnitTests.Services
         }
 
         [Fact]
-        public async Task SHOULD_FIND_BANK_ACCOUNT()
+        public async Task GetAccountAsync_ShouldReturnBankAccount_WhenFound()
         {
-            var bankAccounts = new SBBankAccount();
-            _bankClient.Setup(x => x.GetAccountAsync(It.IsAny<long>(), It.IsAny<long>(), It.IsAny<GetBankAccountRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(bankAccounts);
-            //var response = await _bankClient.GetAccountAsync(new long(), new long(), new GetBankAccountRequest(), default);
-            //Assert.Equal(bankAccounts, response);
+            var bankAccount = new SBBankAccount();
+            _bankClient.Setup(x => x.GetAccountAsync(It.IsAny<long>(), It.IsAny<long>(), It.IsAny<GetBankAccountRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(bankAccount);
+            var response = await _bankClient.Object.GetAccountAsync(new long(), new long(), new GetBankAccountRequest());
+            Assert.Equal(bankAccount, response);
         }
 
         [Fact]
-        public async Task SHOULD_CREATE_BANK()
+        public async Task GetAccountAsync_ShouldThrow_WhenNotFound()
+        {
+            var bank = new SBBank();
+            _bankClient.Setup(x => x.GetAccountAsync(It.IsAny<long>(), It.IsAny<long>(), It.IsAny<GetBankAccountRequest>(), It.IsAny<CancellationToken>())).ThrowsAsync(new HttpRequestException());
+            await Assert.ThrowsAsync<HttpRequestException>(() => _bankClient.Object.GetAccountAsync(new long(), new long(), new GetBankAccountRequest()));
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnBank_WhenCreateSucceed()
         {
             var bank = new SBBank();
             _bankClient.Setup(x => x.CreateAsync(It.IsAny<CreateBankRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(bank);
             var response = await _bankClient.Object.CreateAsync(new CreateBankRequest(), default);
             Assert.Equal(bank, response);
+        }
+        [Fact]
+        public async Task CreateAsync_ShouldThrow_WhenCreateFails()
+        {
+            var bank = new SBBank();
+            _bankClient.Setup(x => x.CreateAsync(It.IsAny<CreateBankRequest>(), It.IsAny<CancellationToken>())).ThrowsAsync(new HttpRequestException());
+            await Assert.ThrowsAsync<HttpRequestException>(() => _bankClient.Object.CreateAsync(new CreateBankRequest(), default));
         }
     }
 }
